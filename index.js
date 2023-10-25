0bb8fa4aeb9831f23d330dcad2c8af9cc5cd36af
Arpit0025 <kumararpit0025@gmail.com>
2023-10-25 22:31:22 +0530

    modified the code

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var cubes = 'ARPIT';
 //styling console output using CSS with a %c format specifier
 for (var i = 0; i < cubes.length; i++) {
     var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: yellow; color: red";
-    console.log("%c" + cubes[i], styles);
+    console.log("%c" + cubes[i], styles);}
